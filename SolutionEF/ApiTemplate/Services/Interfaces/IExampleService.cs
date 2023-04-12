085e98c96301ea1d1e7e053903b8b6d07ef6c1fa
Ibrahim <you@example.com>
2023-04-12 10:07:08 -0500

    Final Version

--- SolutionEF/ApiTemplate/Services/Interfaces/IExampleService.cs
+++ SolutionEF/ApiTemplate/Services/Interfaces/IExampleService.cs
@@ -2,5 +2,11 @@
 {
     public interface IExampleService
     {
+        string Add(object model);
+        string Delete(object model);
+        object Select();
+        object SelectById(object id);
+        string Update(object model);
+        bool ExitsById(object id);
     }
 }
