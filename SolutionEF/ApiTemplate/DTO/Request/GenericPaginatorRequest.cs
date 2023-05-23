3a235d3fcb476fa5556bc39d481fed4ebd411fab
Ibrahim <you@example.com>
2023-05-22 21:35:25 -0500

    Add Service generic CRUD and paginator

---
+++ SolutionEF/ApiTemplate/DTO/Request/GenericPaginatorRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiTemplate.DTO.Request
+{
+    public class GenericPaginatorRequest<T> where T : class
+    {
+        public T? Id { get; set; }
+        [Required]
+        public int Page { get; set; }
+        [Required]
+        public int RecordsByPage { get; set; }
+    }
+}
---
+++ SolutionEF/ApiTemplate/Services/Interfaces/IGenericCRUDService.cs
@@ -0,0 +1,16 @@
+﻿using ApiTemplate.DTO.Respon;
+
+namespace ApiTemplate.Services.Interfaces
+{
+    public interface Interface<A,U,P,C>
+    {
+        Task<GenericRespon> GetFull();
+        Task<GenericRespon> GetByPage(P paginator);
+        Task<GenericRespon> GetByFilter(P filterPaginator);
+        Task<GenericRespon> GetById(C id);
+        Task<GenericRespon> DeleteLogic(C id);
+        Task<GenericRespon> DeletePhysical(C id);
+        Task<GenericRespon> Add(A model);
+        Task<GenericRespon> Update(U model);
+    }
+}
